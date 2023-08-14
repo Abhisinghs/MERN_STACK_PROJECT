db80ab2ec758f501397dd4b11d315b8a0bddc7c1
Abhisinghs <abhisheksingh35716@gmail.com>
2023-08-14 23:51:54 +0530

    config the user module

---
+++ server/routes/user.routes.js
@@ -0,0 +1,14 @@
+//import express to make route
+import express from 'express'
+
+//makes a instance of router
+const routes=express.Router();
+
+//define routes 
+routes.post('/register',register);
+routes.post('/login',login);
+routes.get('/logout',logout);
+routes.get('/me',getProfile);
+
+
+export default routes;
