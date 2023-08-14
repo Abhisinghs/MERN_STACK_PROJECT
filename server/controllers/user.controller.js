b1566e07aab2ced42c3adbfed116af430bb79866
Abhisinghs <abhisheksingh35716@gmail.com>
2023-08-15 00:37:17 +0530

    ready skeleton for user moudle

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,30 @@
+//Here we cover only logic part 
+// In this controller file we only make function or module and use in routers file 
+
+
+
+// define functions 
+const register =async(req,res)=>{
+
+}
+
+const login=async(req,res)=>{
+
+}
+
+const logout = (req,res)=>{
+
+}
+
+const getProfile = (req,res)=>{
+
+}
+
+
+// export these module so routes file can use 
+export {
+    register,
+    login,
+    logout,
+    getProfile
+};
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,8 @@
 //import express to make route
 import express from 'express'
+import register from '../controllers/user.controller.js';
+import login from '../controllers/user.controller.js'
+
 
 //makes a instance of router
 const routes=express.Router();
@@ -7,8 +10,9 @@ const routes=express.Router();
 //define routes for user
 routes.post('/register',register);
 routes.post('/login',login);
-routes.get('/logout',logout);
-routes.get('/me',getProfile);
+// routes.get('/logout',logout);
+// routes.get('/me',getProfile);
+
 
 
 
